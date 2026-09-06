d802768f2e60a2ba1ddf4a1d95e117bfd2238621
otyr <otirmesi@gmail.com>
2026-09-07 02:01:05 +0300

    Creating the overall plan for the demo unix shell

---
+++ prereqs/dem_shell.c
@@ -0,0 +1,25 @@
+int main() {
+
+    
+
+    // print the prompt
+
+    
+
+    // read the command from the user
+
+    
+
+    // split the command into arguments
+
+    
+
+    // create child process running the command
+
+    
+
+    // wait for it to finish
+
+    
+
+}
