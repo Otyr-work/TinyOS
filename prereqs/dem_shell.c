5ffa5c66006b47101f646dd22336bd86a5f7bc99
Otyr-work <otirmesilati2@gmail.com>
2026-09-07 08:55:06 +0300

    Finishing the reading process
    with skeleton logic for the loop

--- prereqs/dem_shell.c
+++ prereqs/dem_shell.c
@@ -1,22 +1,41 @@
-#define IN 0
-#define OUT -1
 #define NO_ERROR 0
 
+// - - - - - - - - - - 
+
+#define IN 0
+#define OUT -1
+
+// - - - - - - - - - -
+
+#define MAX_INPUT 256
+
+
+
 #include <stdio.h>
+#include <string.h>
+
+
+
 
 int main() {
     
-    // create the input string
-    
     printf("shellom$ \n");
     
+    char user_input[MAX_INPUT];
     int flag = IN;
     
     while(flag != OUT) {
-     
-        // check if it is ls, pwd or exit, and change the flag
         
+        fflush(stdout);
+        if (fgets(user_input, sizeof(user_input), stdin) == NULL) flag = OUT;
+        
+        user_input[strcspn(user_input, "\n")] = '\0';
+        
+        if (strcmp(user_input, "pwd") == 0 || strcmp(user_input, "ls") == 0|| strcmp(user_input, "exit") == 0 ) flag = OUT;
+            
     }
+    
+    printf("Done");
     
     // read the command from the user
     
