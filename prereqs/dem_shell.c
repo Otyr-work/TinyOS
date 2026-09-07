8c5448bb8b78536837633ebfd5c0909eda78b160
Otyr-work <otirmesilati2@gmail.com>
2026-09-07 08:35:50 +0300

    adding the defines and sub tasks
    on the way to complete the reading task

--- prereqs/dem_shell.c
+++ prereqs/dem_shell.c
@@ -1,8 +1,22 @@
+#define IN 0
+#define OUT -1
+#define NO_ERROR 0
+
 #include <stdio.h>
 
 int main() {
     
+    // create the input string
+    
     printf("shellom$ \n");
+    
+    int flag = IN;
+    
+    while(flag != OUT) {
+     
+        // check if it is ls, pwd or exit, and change the flag
+        
+    }
     
     // read the command from the user
     
@@ -11,5 +25,7 @@ int main() {
     // create child process running the command
     
     // wait for it to finish
+    
+    return NO_ERROR;
     
 }
