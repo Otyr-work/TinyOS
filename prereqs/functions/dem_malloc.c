8800063c29c686df89f8a778a70b238fb9a2fd6c
Otyr-work <otirmesilati2@gmail.com>
2026-09-12 05:54:36 +0300

    Finishing definition of dem_malloc
    Implement custom memory allocation function dem_malloc using sbrk.

--- prereqs/functions/dem_malloc.c
+++ prereqs/functions/dem_malloc.c
@@ -1,3 +1,20 @@
+#define NO_ERROR 0
+
+#include <unistd.h>
+
+void * dem_malloc(size_t memo_size){
+    
+    void * memo_ptr = sbrk(memo_size);
+    
+    if( memo_ptr == (void *)-1 ) return NULL;
+    
+    return memo_ptr;
+}
+
 int main() {
-  return 0;
+    
+    // testing the dem_malloc
+  
+    return NO_ERROR;
+    
 }
