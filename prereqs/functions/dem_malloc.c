efbf5f559166603fbb8b4725e67b4e7ffe77c1e1
Otyr-work <otirmesilati2@gmail.com>
2026-09-14 00:38:57 +0300

    Adding splitting blocks capability

--- prereqs/functions/dem_malloc.c
+++ prereqs/functions/dem_malloc.c
@@ -3,6 +3,10 @@
 #include <stdio.h> 
 #include <unistd.h> 
 
+
+
+
+
 struct block { 
     
     size_t memo_allocated_size;
@@ -11,19 +15,69 @@ struct block {
     
 };
 
+
+
+
+
 struct block* heap_start = NULL;
+
+
+
+
 
 void* dem_malloc(size_t memo_size){
     
+    // re-use already freed blocks
+    struct block * current_blk = heap_start;
+    
+    
+    
+    while( current_blk != NULL ) {
+        
+        if ( current_blk->free && current_blk->memo_allocated_size >= memo_size )
+        {
+            
+            if (current_blk->memo_allocated_size >= memo_size + sizeof(struct block) + 1) {
+                
+                struct block *new_blk = (struct block *)((char *)(current_blk + 1) + memo_size);
+                
+                new_blk->memo_allocated_size = current_blk->memo_allocated_size - memo_size - sizeof(struct block);
+                
+                new_blk->free = 1;
+                new_blk->next_ptr = current_blk->next_ptr;
+                
+                current_blk->memo_allocated_size = memo_size;
+                current_blk->free = 0;
+                current_blk->next_ptr = new_blk;
+                
+            }
+            
+            else current_blk->free = 0;
+            
+            return(void *)(current_blk + 1);
+        }
+        
+        current_blk = current_blk->next_ptr;
+        
+    }
+    
+    
+    
+    // go over the heap to find a free space 
     struct block *block_ptr = sbrk(sizeof(struct block) + memo_size);
     
+    
     if (block_ptr == (void *)-1) return NULL;
+    
+    
     
     block_ptr->memo_allocated_size = memo_size;
     block_ptr->free = 0;
     block_ptr->next_ptr = NULL;
     
-    if(heap_start == NULL) { heap_start = block_ptr;} 
+    
+    
+    if(heap_start == NULL)  heap_start = block_ptr;
     
     else {
         
@@ -39,6 +93,10 @@ void* dem_malloc(size_t memo_size){
     
 } 
 
+
+
+
+
 void dem_free(void* ptr) { 
     
     struct block *block_ptr = (struct block*)ptr - 1;
@@ -47,16 +105,41 @@ void dem_free(void* ptr) {
     
 }
 
+
+
+
+
 int main() {
 
     int *numbers = dem_malloc(5 * sizeof(int));
-    int *more_numbers = dem_malloc(3 * sizeof(int));
+    int *more_numbers = dem_malloc(16 * sizeof(int));
     int *even_more_numbers = dem_malloc(8 * sizeof(int));
+    
+    
 
     if (numbers == NULL || more_numbers == NULL || even_more_numbers == NULL)
         return 1;
 
+    
+
     dem_free(more_numbers);
+    
+    
+    
+    int *reused_numbers = dem_malloc(2*sizeof(int));
+    
+    
+    
+    if(reused_numbers == NULL) return 1;
+    
+    printf("Original freed address: %p\n", (void *)more_numbers);
+    printf("reused addresses: %p\n", (void *)reused_numbers);
+    
+    
+    
+    printf("\nBookkeeping:\n");
+
+    
 
     struct block *current_blk = heap_start;
 
