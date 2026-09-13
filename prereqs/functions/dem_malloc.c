e15ebe1ad549f1c7096218570b1eed88b1bb41d9
Otyr-work <otirmesilati2@gmail.com>
2026-09-13 10:09:20 +0300

    Adding memory allocation sequencing + testing

--- prereqs/functions/dem_malloc.c
+++ prereqs/functions/dem_malloc.c
@@ -1,20 +1,77 @@
 #define NO_ERROR 0
 
-#include <unistd.h>
+#include <stdio.h> 
+#include <unistd.h> 
 
-void * dem_malloc(size_t memo_size){
+struct block { 
     
-    void * memo_ptr = sbrk(memo_size);
+    size_t memo_allocated_size;
+    int free;
+    struct block* next_ptr;
     
-    if( memo_ptr == (void *)-1 ) return NULL;
+};
+
+struct block* heap_start = NULL;
+
+void* dem_malloc(size_t memo_size){
     
-    return memo_ptr;
+    struct block *block_ptr = sbrk(sizeof(struct block) + memo_size);
+    
+    if (block_ptr == (void *)-1) return NULL;
+    
+    block_ptr->memo_allocated_size = memo_size;
+    block_ptr->free = 0;
+    block_ptr->next_ptr = NULL;
+    
+    if(heap_start == NULL) { heap_start = block_ptr;} 
+    
+    else {
+        
+        struct block * current_blk = heap_start;
+        
+        while (current_blk->next_ptr != NULL) current_blk=current_blk->next_ptr;
+        
+        current_blk->next_ptr = block_ptr;
+        
+    }
+    
+    return (void *)(block_ptr + 1);
+    
+} 
+
+void dem_free(void* ptr) { 
+    
+    struct block *block_ptr = (struct block*)ptr - 1;
+    
+    block_ptr->free = 1;
+    
 }
 
 int main() {
-    
-    // testing the dem_malloc
-  
+
+    int *numbers = dem_malloc(5 * sizeof(int));
+    int *more_numbers = dem_malloc(3 * sizeof(int));
+    int *even_more_numbers = dem_malloc(8 * sizeof(int));
+
+    if (numbers == NULL || more_numbers == NULL || even_more_numbers == NULL)
+        return 1;
+
+    dem_free(more_numbers);
+
+    struct block *current_blk = heap_start;
+
+    while (current_blk != NULL) {
+
+        printf(
+            "block: %p | size: %zu | free: %d | next: %p\n",
+            (void *)current_blk,
+            current_blk->memo_allocated_size,
+            current_blk->free,
+            (void *)current_blk->next_ptr
+        );
+
+        current_blk = current_blk->next_ptr;
+    }
+
     return NO_ERROR;
-    
 }
