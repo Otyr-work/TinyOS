5fe8f3c5869836cc14b8a4b8bb553a486f3cce5a
Otyr-work <otirmesilati2@gmail.com>
2026-09-25 10:46:47 +0300

    first part of the locking mechanism

--- prereqs/functions/threadpool.c
+++ prereqs/functions/threadpool.c
@@ -5,13 +5,20 @@
 #include <sys/syscall.h>
 #include <unistd.h>
 
+#define WORKER_COUNT 3
+#define JOB_COUNT 6
+
 struct job {
-    
+  
     void (*function)(void *);
     void * argument;
     
 };
 
+struct job jobs[JOB_COUNT];
+int next_job = 0;
+
+pthread_mutex_t queue_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 void print_function(void * args) {
     
@@ -23,19 +30,23 @@ void print_function(void * args) {
 
 void * worker (void * args) {
     
+    int flag interrupt_queue = 0;
+    
+    while (interrupt_queue != -1){
+        
+        // continue from here
+        
+    }
+    
     struct job * curr_job = (struct job *)args;
-
-   curr_job->function(curr_job->argument);
-
+    
+    curr_job->function(curr_job->argument);
+    
     printf("Thread ID: %ld\n", syscall(SYS_gettid));
-
+    
     return NULL;
-
+    
 }
-
-
-
-
 
 int main() {
     
@@ -43,15 +54,17 @@ int main() {
     int number_2 = 100;
     int number_3 = 777;
     
-    
-    struct job jobs[3] = { 
-    { .function = print_function, .argument = &number_1 },
-    { .function = print_function, .argument = &number_2 },
-    { .function = print_function, .argument = &number_3 }
-    
+    struct job jobs[3] = {
+        
+        { .function = print_function, .argument = &number_1 },
+        { .function = print_function, 
+        .argument = &number_2 },    
+        { .function = print_function,
+        .argument = &number_3 }
+        
     };
-
-    for (int i = 0; i < 3; i++ ) {
+    
+    for (int i = 0; i < 3; i++) {
         
         pthread_t thread;
         
@@ -60,13 +73,11 @@ int main() {
         pthread_join(thread, NULL);
         
     }
-
     
     printf("Thread ID: %ld\n", syscall(SYS_gettid));
-
-
-    printf("Meain thread done\n\n");
-
+    
+    printf("Main thread done\n\n");
+    
     return NO_ERROR;
-
+    
 }
